44203dac81b6166345214e63b064f9491d7b278a
Sau <110614172+saurabhTandale@users.noreply.github.com>
2023-03-05 18:54:13 +0530

    netflix-clone.github.io

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
+import Navbar from './Navbar';
+
+// npm install firebase  
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />  
+      <Banner />
+
+   <Row className="large" title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+       />
+
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="TopRated" fetchUrl={requests.fetchTopRated} />
+      <Row title="ActionMovies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="ComedyMovies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="HorrorMovies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="RomanceMovies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      
+      
+
+     
+
+    
+
+    </div>
+  );
+}
+
+export default App;
